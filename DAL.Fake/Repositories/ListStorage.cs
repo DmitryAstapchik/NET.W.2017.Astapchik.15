6375e8c48e536653fccdd534af81d9501fdbe1b5
Dmitriy Astapchik <32869697+DmitriyAstapchik@users.noreply.github.com>
2017-12-17 16:17:06 +0300

    Add files via upload

--- DAL.Fake/Repositories/ListStorage.cs
+++ DAL.Fake/Repositories/ListStorage.cs
@@ -9,7 +9,12 @@ namespace DAL.Fake
 {
     public class ListStorage : IAccountRepository
     {
-        private List<AccountDTO> storage = new List<AccountDTO>();
+        private List<AccountDTO> storage = new List<AccountDTO>
+        {
+            new AccountDTO("111", "owner1", 100, 23.34f, AccountDTO.AccountType.GoldAccount),
+            new AccountDTO("222", "owner2", 200, 0.55f, AccountDTO.AccountType.PlatinumAccount),
+               new AccountDTO("123", "owner1", 300, 34.34f, AccountDTO.AccountType.StandardAccount)
+        };
 
         public void Create(AccountDTO account)
         {
@@ -29,7 +34,7 @@ namespace DAL.Fake
         public void Delete(AccountDTO account)
         {
             if (!storage.Remove(account))
-            {         
+            {
                 throw new ApplicationException($"account with IBAN {account.IBAN} was not removed");
             }
         }
@@ -46,6 +51,11 @@ namespace DAL.Fake
             {
                 throw new ApplicationException($"account with IBAN { account.IBAN } was not found");
             }
+        }
+
+        public IEnumerable<AccountDTO> GetAllAccounts()
+        {
+            return this.storage;
         }
     }
 }
